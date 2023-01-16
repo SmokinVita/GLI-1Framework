a2e68e017766a8045937649d0bfb56c01a761aa1
SmokinVita <69760636+SmokinVita@users.noreply.github.com>
2023-01-16 22:14:36 +0900

    Barriers can be shot, deactivated, and reactivated.

--- Assets/Scripts/Barriers.cs
+++ Assets/Scripts/Barriers.cs
@@ -6,10 +6,35 @@ public class Barriers : MonoBehaviour, IDamageable
 {
 
     [SerializeField]
-    private int _health;
-    
+    private int _health = 3;
+    [SerializeField]
+    private GameObject _forceField;
+    private Collider _collider;
+
+    void Start()
+    {
+        _collider = GetComponent<Collider>();
+        if (_collider == null)
+            Debug.LogError("Collider is NULL on " + this.gameObject);
+    }
+
     public void Damage()
     {
-        throw new System.NotImplementedException();
+        _health--;
+        Debug.Log("Barrier Got Hit!");
+        if(_health <= 0)
+        {
+            _forceField.SetActive(false);
+            _collider.enabled = false;
+            StartCoroutine(RechargeRoutine());
+        }
+    }
+
+    IEnumerator RechargeRoutine()
+    {
+        yield return new WaitForSeconds(5f);
+        _health = 3;
+        _forceField.SetActive(true);
+        _collider.enabled = true;
     }
 }
